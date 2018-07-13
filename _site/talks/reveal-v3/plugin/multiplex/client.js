c0e748eb5893823909764c2bb40d150140cdc959
Gavin Ha <gavinha@gmail.com>
2018-07-13 16:55:28 -0700

    remove _site

--- _site/talks/reveal-v3/plugin/multiplex/client.js
+++
@@ -1,1 +0,0 @@
-!function(){var t=Reveal.getConfig().multiplex,o=t.id,e=io.connect(t.url);e.on(t.id,function(t){t.socketId===o&&"localhost:1947"!==window.location.host&&Reveal.setState(t.state)})}();
